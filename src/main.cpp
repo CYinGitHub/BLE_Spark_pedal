5574ec1b55c19da2a5ba829338a7822befe2f844
copych <ecopy@ya.ru>
2021-05-03 02:27:03 +0300

    Some UI added

--- src/main.cpp
+++ src/main.cpp
@@ -10,25 +10,31 @@
 #endif
 #include "OLEDDisplayUi.h"
 #include "images.h"
+#include "fonts.h"
 
 #define FORMAT_LITTLEFS_IF_FAILED true
 #include "LITTLEFS.h"
 
 #ifdef DEBUG_ENABLE
-#define DEBUG(x) Serial.println(x)
+#define DEBUG(x) Serial.println((String)(millis()/1000) + "s. " + x)
 #else
 #define DEBUG(x)
 #endif
 
 /* GENERAL AND GLOBALS ======================================================================= */
+enum e_mode {MODE_CONNECT, MODE_EFFECTS, MODE_PRESETS, MODE_ABOUT}; // these numbers also correspond to frame numbers of the UI
+e_mode mode = MODE_CONNECT;
 const char* DEVICE_NAME = "Easy Spark"; 
 const char* VERSION = "0.1a"; 
 const char* SPARK_BT_NAME = "Spark 40 Audio";
+unsigned long countBT = 0;
+unsigned long countUI = 0;
+unsigned long countBlink = 0;
+bool btConnected = false;
+int localPreset;
 
 /* BUTTONS Init ============================================================================== */
 const uint8_t BUTTONS_NUM = 4;
-unsigned long countBT = 0;
-unsigned long countUI = 0;
 
 struct s_buttons {
   const uint8_t pin;
@@ -57,66 +63,48 @@ void handleEvent(ace_button::AceButton*, uint8_t, uint8_t); //forward ref
 OLEDDisplayUi ui( &display );
 
 void msOverlay(OLEDDisplay *display, OLEDDisplayUiState* state) {
-  display->setTextAlignment(TEXT_ALIGN_RIGHT);
-  display->setFont(ArialMT_Plain_10);
-  display->drawString(128, 0, String(millis()));
+  if (!btConnected) {
+ /*   if(round(millis()/400)*400 != round(millis()/300)*300 ) {
+      display->drawXbm(display->width()-7, 0, small_bt_logo_width, small_bt_logo_height, small_bt_logo_bits);
+    } */
+  } else { 
+    display->drawXbm(display->width()-7, 0, small_bt_logo_width, small_bt_logo_height, small_bt_logo_bits);
+  } 
 }
 
-void drawFrame1(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
-  // draw an xbm image.
-  display->drawXbm((display->width()-BT_Logo_width)/2 + x, (display->height()-BT_Logo_height)/2 + y, BT_Logo_width, BT_Logo_height, BT_Logo_bits);
-}
-
-void drawFrame2(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
-  // Demonstrates the 3 included default sizes. The fonts come from SSD1306Fonts.h file
-  // Besides the default fonts there will be a program to convert TrueType fonts into this format
-  display->setTextAlignment(TEXT_ALIGN_LEFT);
-  display->setFont(ArialMT_Plain_10);
-  display->drawString(0 + x, 10 + y, "Arial 10");
-
-  display->setFont(ArialMT_Plain_16);
-  display->drawString(0 + x, 20 + y, "Arial 16");
-
-  display->setFont(ArialMT_Plain_24);
-  display->drawString(0 + x, 34 + y, "Arial 24");
-}
-
-void drawFrame3(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
-  // Text alignment demo
-  display->setFont(ArialMT_Plain_10);
-
-  // The coordinates define the left starting point of the text
-  display->setTextAlignment(TEXT_ALIGN_LEFT);
-  display->drawString(0 + x, 11 + y, "Left aligned (0,10)");
-
-  // The coordinates define the center of the text
+void frameBtConnect(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
+  display->drawXbm((display->width()-BT_Logo_width)/2 + x, 16, BT_Logo_width, BT_Logo_height, BT_Logo_bits);
   display->setTextAlignment(TEXT_ALIGN_CENTER);
-  display->drawString(64 + x, 22 + y, "Center aligned (64,22)");
-
-  // The coordinates define the right end of the text
-  display->setTextAlignment(TEXT_ALIGN_RIGHT);
-  display->drawString(128 + x, 33 + y, "Right aligned (128,33)");
-}
-
-void drawFrame4(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
-  // Demo for drawStringMaxWidth:
-  // with the third parameter you can define the width after which words will be wrapped.
-  // Currently only spaces and "-" are allowed for wrapping
-  display->setTextAlignment(TEXT_ALIGN_LEFT);
   display->setFont(ArialMT_Plain_10);
-  display->drawStringMaxWidth(0 + x, 10 + y, 128, "Lorem ipsum\n dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore.");
+  display->drawString(display->width()/2 + x,  y, "CONNECTING...");
 }
 
-void drawFrame5(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
+void frameEffects(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
+
+  display->setFont(DSEG14_Classic_Bold_48);
+  display->setTextAlignment(TEXT_ALIGN_CENTER);
+  display->drawString(64 + x, 11 + y, String("18B.") );
 
 }
 
-// This array keeps function pointers to all frames
-// frames are the single views that slide in
-FrameCallback frames[] = { drawFrame1, drawFrame2, drawFrame3, drawFrame4, drawFrame5 };
+void framePresets(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
+  display->setFont(DSEG14_Classic_Bold_48);
+  display->setTextAlignment(TEXT_ALIGN_CENTER);
+  display->drawString(64 + x, 17 + y, String(localPreset) );
+}
 
-// how many frames are there?
-int frameCount = 5;
+void frameAbout(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
+  display->setFont(ArialMT_Plain_16);
+  display->setTextAlignment(TEXT_ALIGN_CENTER);
+  display->drawString(display->width()/2 + x, 14 + y, DEVICE_NAME);
+  display->drawString(display->width()/2 + x, 36 + y, VERSION);
+}
+
+// array of frame drawing functions
+FrameCallback frames[] = { frameBtConnect, frameEffects, framePresets, frameAbout };
+
+// number of frames in UI
+int frameCount = 4;
 
 // Overlays are statically drawn on top of a frame eg. a clock
 OverlayCallback overlays[] = { msOverlay };
@@ -127,7 +115,6 @@ int overlaysCount = 1;
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
-bool btConnected = false;
 BluetoothSerial SerialBT;
 void btConnect(); // Forward ref
 void btInit(); // Forward ref
@@ -139,33 +126,21 @@ void setup() {
   Serial.begin(115200);
   DEBUG(F("Serial started"));
 #endif  
-
+  localPreset = 1;
   ui.setTargetFPS(30);
-
-	// Customize the active and inactive symbol
-  ui.setActiveSymbol(activeSymbol);
-  ui.setInactiveSymbol(inactiveSymbol);
-
-  // You can change this to
-  // TOP, LEFT, BOTTOM, RIGHT
-  ui.setIndicatorPosition(BOTTOM);
-
-  // Defines where the first frame is located in the bar.
-  ui.setIndicatorDirection(LEFT_RIGHT);
-
-  // You can change the transition that is used
-  // SLIDE_LEFT, SLIDE_RIGHT, SLIDE_UP, SLIDE_DOWN
+  ui.disableAllIndicators();
   ui.setFrameAnimation(SLIDE_LEFT);
-
-  // Add frames
   ui.setFrames(frames, frameCount);
-
-  // Add overlays
   ui.setOverlays(overlays, overlaysCount);
-
+  ui.disableAutoTransition();
   // Initialising the UI will init the display too.
   ui.init();
   display.flipScreenVertically();
+  ui.switchToFrame(MODE_ABOUT);// show welcome screen
+  ui.update();
+  delay(1000);
+  ui.switchToFrame(MODE_CONNECT);
+  ui.update();
 
   for (uint8_t i = 0; i < BUTTONS_NUM; i++) {
     pinMode(BUTTONS[i].pin, INPUT_PULLUP);
@@ -174,12 +149,11 @@ void setup() {
     //..........
   }
 
-  // Configure the ButtonConfig with the event handler, and enable all higher
-  // level events.
+  // Configure the ButtonConfig with the event handler, and enable all higher level events.
   ace_button::ButtonConfig* buttonConfig = ace_button::ButtonConfig::getSystemButtonConfig();
-  // just two events for now
   buttonConfig->setEventHandler(handleEvent);
   buttonConfig->setFeature(ace_button::ButtonConfig::kFeatureClick);
+  buttonConfig->setFeature(ace_button::ButtonConfig::kFeatureDoubleClick);
   buttonConfig->setFeature(ace_button::ButtonConfig::kFeatureLongPress);
 
   btInit();
@@ -219,25 +193,30 @@ void handleEvent(ace_button::AceButton* button, uint8_t eventType, uint8_t butto
   uint8_t id = button->getId();
   DEBUG("handleEvent(): id: " + (String)id + " eventType: " + (String)eventType + "; buttonState: " + (String)buttonState );
 
-/*  uint8_t ledPin = BUTTONS[id].ledPin;
-
-  // Control the LED only for the Pressed and Released events.
-  // Notice that if the MCU is rebooted while the button is pressed down, no
-  // event is triggered and the LED remains off.
-  switch (eventType) {
-    case ace_button::AceButton::kEventPressed:
-      digitalWrite(ledPin, LED_ON);
-      BUTTONS[id].ledState = LED_ON;
-      break;
-    case AceButton::kEventReleased:
-      digitalWrite(ledPin, LED_OFF);
-      INFOS[id].ledState = LED_OFF;
-      break;
+//  uint8_t ledPin = BUTTONS[id].ledAddr;
+  if (mode==MODE_EFFECTS) {
+    switch (eventType) {
+      case ace_button::AceButton::kEventPressed:
+        break;
+      case ace_button::AceButton::kEventLongPressed:
+        break;
+      case ace_button::AceButton::kEventDoubleClicked:
+        break;
+    }
   }
-  */
+  if (mode==MODE_PRESETS){
+    switch (eventType) {
+      case ace_button::AceButton::kEventPressed:
+        break;
+      case ace_button::AceButton::kEventLongPressed:
+        break;
+      case ace_button::AceButton::kEventDoubleClicked:
+        break;
+    }
+  }
 }
 
-//Bluetooth Event Call Back
+//on Bluetooth event
 void btEventCallback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param) {
   // On BT connection close
   if (event == ESP_SPP_CLOSE_EVT ) {
@@ -253,14 +232,12 @@ void btInit() {
   // Register BT event callback method
   SerialBT.register_callback(btEventCallback);
   if (!SerialBT.begin(DEVICE_NAME, true)) { // Detect for BT failure on ESP32 chip
-    // Show "BT Init Failed!" message on device screen
     display.clear();
-    display.setFont(ArialMT_Plain_24);
+    display.setFont(ArialMT_Plain_16);
     display.setTextAlignment(TEXT_ALIGN_CENTER);
-    display.drawString(64, 6, "BT Init");
-    display.drawString(64, 30, "Failed!");
+    display.drawString(display.width()/2, 13, "Bluetooth");
+    display.drawString(display.width()/2, 36, "Not Available!");
     display.display();
-
     // Loop infinitely until device shutdown/restart
     while (true) {};
   }
@@ -271,10 +248,16 @@ void btInit() {
 void btConnect() {
   // Loop until device establishes connection with amp
   while (!btConnected) {
-    DEBUG("Connecting");
+    DEBUG("Connecting...");
+    ui.switchToFrame(MODE_CONNECT);
+    ui.update();
     btConnected = SerialBT.connect(SPARK_BT_NAME);
+    delay(500);
     // If BT connection with amp is successful
     if (btConnected && SerialBT.hasClient()) {
+      mode = MODE_EFFECTS;
+      ui.switchToFrame(mode);
+      ui.update();
       DEBUG("BT Connected");
     } else {
       // in case of some connection loss

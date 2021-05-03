acb415ba09257185968c3896cc58a5082c174774
copych <ecopy@ya.ru>
2021-05-03 19:32:28 +0300

    Added encoders

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <Arduino.h>
 #include <FS.h>
 #include "BluetoothSerial.h" 
-#include "AceButton.h" 
+#include "AceButton.h"
+#include "MD_REncoder.h"
 #ifdef SSD1306WIRE //these global def's are in platformio.ini
   #include "SSD1306Wire.h"
 #endif
@@ -22,21 +23,33 @@
 #endif
 
 /* GENERAL AND GLOBALS ======================================================================= */
-enum e_mode {MODE_CONNECT, MODE_EFFECTS, MODE_PRESETS, MODE_ABOUT}; // these numbers also correspond to frame numbers of the UI
+enum e_mode {MODE_CONNECT, MODE_EFFECTS, MODE_PRESETS, MODE_ABOUT, MODE_LEVEL}; // these numbers also correspond to frame numbers of the UI
 e_mode mode = MODE_CONNECT;
-const char* DEVICE_NAME = "Easy Spark"; 
+e_mode returnFrame = MODE_EFFECTS;
+const unsigned long FRAME_TIMEOUT = 2000; //(ms) to return to main ui  
+const char* DEVICE_NAME = "Easy Spark";
 const char* VERSION = "0.1a"; 
 const char* SPARK_BT_NAME = "Spark 40 Audio";
+const uint8_t TOTAL_PRESETS = 100; // number of stored on board presets
+const uint8_t MAX_LEVEL = 100; // number of stored on board presets
 unsigned long countBT = 0;
 unsigned long countUI = 0;
 unsigned long countBlink = 0;
+bool tempUI = false;
 bool btConnected = false;
-int localPreset;
+uint8_t localPreset;
+int level = 0;
+unsigned long timeToGoBack;
+
+/* Forward declarations ====================================================================== */
+void tempFrame(e_mode tempFrame, e_mode returnFrame, const unsigned long msTimeout) ;
+void handleEvent(ace_button::AceButton*, uint8_t, uint8_t);
+void btConnect();
+void btInit();
+void returnToMainUI(); 
 bool blinkOn() {if(round(millis()/400)*400 != round(millis()/300)*300 ) return true; else return false;}
 
-
 /* BUTTONS Init ============================================================================== */
-const uint8_t BUTTONS_NUM = 4;
 
 struct s_buttons {
   const uint8_t pin;
@@ -46,15 +59,25 @@ struct s_buttons {
   uint32_t ledState; //data type may change, as i read the docs, enum of selected rgb colors maybe
 };
 
+const uint8_t BUTTONS_NUM = 6;
+const uint8_t PEDALS_NUM = 4; //  first N buttons which are not encoders 
+
 s_buttons BUTTONS[BUTTONS_NUM] = {
   {25, "DRV", "DLO", 0, 0},
   {26, "MOD", "SAV", 0, 0},
   {27, "DLY", "PRV", 0, 0},
-  {14, "RVB", "NXT", 0, 0}
+  {14, "RVB", "NXT", 0, 0},
+  {19, "VOL", "MAS", 0, 0}, //encoder 1 (may or may not be here)
+  {17, "PRS", "PRS", 0, 0}, //encoder 2 (may or may not be here)
 };
 
 ace_button::AceButton buttons[BUTTONS_NUM];
-void handleEvent(ace_button::AceButton*, uint8_t, uint8_t); //forward ref
+
+
+/* ENCODERS Init ============================================================================= */
+MD_REncoder Encoder1 = MD_REncoder(18, 5);
+MD_REncoder Encoder2 = MD_REncoder(16, 4);
+
 
 /* DISPLAY Init ============================================================================== */
 #ifdef SSD1306WIRE
@@ -85,27 +108,25 @@ void frameEffects(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, in
   display->drawString(64 + x, 11 + y, String(localPreset) );
   
   display->setFont(ArialMT_Plain_10);
-  int pxPerLabel = (display->width() - 8) / BUTTONS_NUM;
+  int pxPerLabel = (display->width() - 8) / PEDALS_NUM;
   int boxWidth = display->getStringWidth("WWW")+1;
-  for (int i=0 ; i<BUTTONS_NUM; i++) {
+  for (int i=0 ; i<PEDALS_NUM; i++) {
     display->fillRect(x+(i*pxPerLabel+(pxPerLabel-boxWidth)/2),y,boxWidth,13);
     display->drawString(x+((i+0.5)*pxPerLabel),y,(BUTTONS[i].efxLabel));
   }
-
 }
 
 void framePresets(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
-  display->setFont(Roboto_Mono_Medium_52);
-  display->setTextAlignment(TEXT_ALIGN_CENTER);
-  display->drawString(64 + x, 17 + y, String(localPreset) );
-  
   display->setFont(ArialMT_Plain_10);
-  int pxPerLabel = (display->width() - 8) / BUTTONS_NUM;
+  int pxPerLabel = (display->width() - 8) / PEDALS_NUM;
   int boxWidth = display->getStringWidth("WWW")+1;
-  for (int i=0 ; i<BUTTONS_NUM; i++) {
+  for (int i=0 ; i<PEDALS_NUM; i++) {
     display->fillRect(x+(i*pxPerLabel+(pxPerLabel-boxWidth)/2),y,boxWidth,13);
     display->drawString(x+((i+0.5)*pxPerLabel),y,(BUTTONS[i].actLabel));
   }
+  display->setFont(Roboto_Mono_Medium_52);
+  display->setTextAlignment(TEXT_ALIGN_CENTER);
+  display->drawString(64 + x, 17 + y, String(localPreset) );
 }
 
 void frameAbout(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
@@ -115,11 +136,21 @@ void frameAbout(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int1
   display->drawString(display->width()/2 + x, 36 + y, VERSION);
 }
 
+void frameLevel(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
+  display->setFont(ArialMT_Plain_10);
+  display->setTextAlignment(TEXT_ALIGN_CENTER);
+  display->drawString(display->width()/2 + x,  y, "VOLUME");
+  
+  display->setFont(Roboto_Mono_Medium_52);
+  display->setTextAlignment(TEXT_ALIGN_CENTER);
+  display->drawString(64 + x, 11 + y, String(level) );
+} 
+
 // array of frame drawing functions
-FrameCallback frames[] = { frameBtConnect, frameEffects, framePresets, frameAbout };
+FrameCallback frames[] = { frameBtConnect, frameEffects, framePresets, frameAbout, frameLevel };
 
 // number of frames in UI
-int frameCount = 4;
+int frameCount = 5;
 
 // Overlays are statically drawn on top of a frame eg. a clock
 OverlayCallback overlays[] = { msOverlay };
@@ -131,8 +162,6 @@ int overlaysCount = 1;
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 BluetoothSerial SerialBT;
-void btConnect(); // Forward ref
-void btInit(); // Forward ref
 
 
 /* SETUP() WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW */
@@ -166,6 +195,10 @@ void setup() {
     //..........
   }
 
+  //Start rotary encoders
+  Encoder1.begin();
+  Encoder2.begin();
+
   // Configure the ButtonConfig with the event handler, and enable all higher level events.
   ace_button::ButtonConfig* buttonConfig = ace_button::ButtonConfig::getSystemButtonConfig();
   buttonConfig->setEventHandler(handleEvent);
@@ -196,6 +229,34 @@ void loop() {
     if (SerialBT.available()) {
       SerialBT.read();
     }
+    uint8_t x ;
+    uint16_t s;
+    x = Encoder1.read();
+    s = Encoder1.speed();
+    if (x) {
+      s = s/7 + 1;
+      //DEBUG((String)s);
+      tempFrame(MODE_LEVEL,mode,FRAME_TIMEOUT);
+      if (x == DIR_CW) {
+        level = level + s;
+        if (level>MAX_LEVEL) level = MAX_LEVEL;
+      } else {
+        level = level - s;
+        if (level<0) level=0;
+      }
+    }
+    x = Encoder2.read();
+    if (x) {
+      returnToMainUI();
+      if (x == DIR_CW) {
+        localPreset++;
+        if (localPreset>TOTAL_PRESETS) localPreset = 1;
+      } else {
+        localPreset--;
+        if (localPreset<1) localPreset=TOTAL_PRESETS;
+      }
+    }
+    
     //keepalive for Spark amp BT connection
   /*  if (millis() - countBT > 10000) {
       // request serial number and read returned bytes and discard - keep-alive link to Spark
@@ -205,18 +266,25 @@ void loop() {
     }
   */
   }
+  if ((millis() > timeToGoBack) && tempUI) {
+    returnToMainUI();
+  }
   
 }
 
 /* CUSTOM FUNCTIONS =============================================================================== */
 void handleEvent(ace_button::AceButton* button, uint8_t eventType, uint8_t buttonState) {
   uint8_t id = button->getId();
+  if (id != 4) {returnToMainUI();}
   DEBUG("handleEvent(): id: " + (String)id + " eventType: " + (String)eventType + "; buttonState: " + (String)buttonState );
 
 //  uint8_t ledPin = BUTTONS[id].ledAddr;
   if (mode==MODE_EFFECTS) {
     switch (eventType) {
       case ace_button::AceButton::kEventPressed:
+        if (id==4) {
+          tempFrame(MODE_LEVEL,mode,FRAME_TIMEOUT);
+        }
         break;
       case ace_button::AceButton::kEventLongPressed:
         if (id==3) {
@@ -300,4 +368,21 @@ void btConnect() {
       DEBUG("BT NOT Connected");
     }
   }
+}
+
+void tempFrame(e_mode tempFrame, e_mode retFrame, const unsigned long msTimeout) {
+  if (!tempUI) {
+    mode = tempFrame;
+    ui.switchToFrame(mode);
+    returnFrame = retFrame;
+    tempUI = true;
+  }
+  timeToGoBack = millis() + msTimeout; 
+}
+
+void returnToMainUI() {
+  mode = returnFrame;
+  ui.switchToFrame(mode);
+  timeToGoBack = millis();
+  tempUI = false;
 }

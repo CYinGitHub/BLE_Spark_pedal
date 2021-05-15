5094e1e78ffb86751bfe7ef9244f893ab2bb2dea
copych <ecopy@ya.ru>
2021-05-15 22:36:54 +0300

    revert to previous ver

--- lib/SparkIO/SparkIO.cpp
+++ lib/SparkIO/SparkIO.cpp
@@ -120,7 +120,7 @@ uint8_t chunk_header_from_spark[16]{0x01, 0xfe, 0x00, 0x00, 0x41, 0xff, 0x00, 0x
 
 void SparkIO::process_in_blocks() {
   uint8_t b;
-  //bool boo;
+  bool boo;
 
   while (comms->bt->available()) {
     b = comms->bt->read();
@@ -816,7 +816,7 @@ void SparkIO::create_preset(SparkPreset *preset)
 
 void SparkIO::out_store(uint8_t b)
 {
- // uint8_t bits;
+  uint8_t bits;
   
   if (oc_bit_mask == 0x80) {
     oc_bit_mask = 1;
@@ -844,8 +844,7 @@ void SparkIO::out_store(uint8_t b)
 
 
 void SparkIO::process_out_chunks() {
-  int i, len;
-  //int j;
+  int i, j, len;
   int checksum_pos;
   uint8_t b;
   uint8_t len_h, len_l;
@@ -926,9 +925,9 @@ void SparkIO::process_out_chunks() {
 
 void SparkIO::process_out_blocks() {
   int i;
- // int len;
+  int len;
   uint8_t b;  
-  uint8_t cmd=0, sub=0;
+  uint8_t cmd, sub;
 
   while (!out_chunk.is_empty() && ob_ok_to_send) {
     ob_pos = 16;
